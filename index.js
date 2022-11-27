bbce787501f0d6e03561da61d8ad9aaefff72a49
mitHSwas <wdev.mithunbiswas@gmail.com>
2022-11-28 03:48:32 +0600

    category wise product calling api generated successfully

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config()
 const port = process.env.PORT || 5000;
@@ -15,11 +15,18 @@ const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology:
 async function run() {
     try {
         const accessoriesCategoryCollection = client.db("handToHand").collection("accessoriesCategories");
+        const allAccessoryCollection = client.db("handToHand").collection("allAccessory");
 
         app.get('/categories', async (req, res) => {
             const query = {};
             const cursor = await accessoriesCategoryCollection.find(query).toArray();
             res.send(cursor)
+        })
+        app.get('/category/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { categoryId: id };
+            const result = await allAccessoryCollection.find(query).toArray();
+            res.send(result);
         })
     }
     finally {
