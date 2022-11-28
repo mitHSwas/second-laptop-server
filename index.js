fa2f2ff4d10706c251f067dca6eaee3bc7ca6df8
mitHSwas <wdev.mithunbiswas@gmail.com>
2022-11-29 04:23:22 +0600

    booking product and user is seving to the db

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ async function run() {
     try {
         const accessoriesCategoryCollection = client.db("handToHand").collection("accessoriesCategories");
         const allAccessoryCollection = client.db("handToHand").collection("allAccessory");
+        const bookingsCollection = client.db("handToHand").collection("booking");
+        const usersCollection = client.db("handToHand").collection("users");
 
         app.get('/categories', async (req, res) => {
             const query = {};
@@ -26,6 +28,16 @@ async function run() {
             const id = req.params.id;
             const query = { categoryId: id };
             const result = await allAccessoryCollection.find(query).toArray();
+            res.send(result);
+        })
+        app.post('/booking', async (req, res) => {
+            const info = req.body;
+            const result = await bookingsCollection.insertOne(info);
+            res.send(result);
+        })
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
             res.send(result);
         })
     }
